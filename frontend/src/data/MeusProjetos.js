04bf657d93898fad054cd5f82399d9d21884fbc1
Lucas Engineer <lucas.software.engineering@gmail.com>
2026-01-18 06:41:58 -0300

    data alignment + navigation between screens in the navigation bar + icon on the website tab

--- frontend/src/data/MeusProjetos.js
+++ frontend/src/data/MeusProjetos.js
@@ -2,122 +2,62 @@ const MyProjects = [
     {
         id: 1,
         nav: {
-            url_logo: "https://raw.githubusercontent.com/LucasfcBastos/custom_icons/main/Portfolio/Happy%20Paws/logo.png",
-            name_project: "Happy Paws",
-            tech_principal: "Tecnologia Social",
-            setor: "Saúde e Bem Estar",
-            ods: "15",
+            logo_url: "https://raw.githubusercontent.com/LucasfcBastos/custom_icons/main/Portfolio/Happy%20Paws/logo.png",
+            project_name: "Happy Paws",
+            main_tech: "Tecnologia da Informação e Telecomunicações",
+            sector: "Saúde e Bem Estar",
+            sdg: "3",
         },
-        img_projet: "https://raw.githubusercontent.com/LucasfcBastos/custom_icons/main/Portfolio/Happy%20Paws/tela.png",
-        oportunidade: "Text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text.",
-        solucao: "Text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text.",
-        tech: [
-            {
-                type: "Word",
-                url: "https://raw.githubusercontent.com/LucasfcBastos/custom_icons/main/version01/word.svg"
-            },
-            {
-                type: "Bizagi Modeler",
-                url: "https://raw.githubusercontent.com/LucasfcBastos/custom_icons/main/version01/bizagi-modeler.svg"
-            },
-            {
-                type: "MySQL Workbench",
-                url: "https://raw.githubusercontent.com/LucasfcBastos/custom_icons/main/version01/mysql-workbench.svg"
-            },
-            {
-                type: "Foss Flow",
-                url: "https://raw.githubusercontent.com/LucasfcBastos/custom_icons/main/version01/foss-flow.svg"
-            },
-            {
-                type: "Figma",
-                url: "https://raw.githubusercontent.com/LucasfcBastos/custom_icons/main/version01/figma.svg"
-            },
-            {
-                type: "Flaticon",
-                url: "https://raw.githubusercontent.com/LucasfcBastos/custom_icons/main/version01/flaticon.svg"
-            },
-            {
-                type: "Storyset",
-                url: "https://raw.githubusercontent.com/LucasfcBastos/custom_icons/main/version01/storyset.svg"
-            },
-            {
-                type: "GitHub",
-                url: "https://raw.githubusercontent.com/LucasfcBastos/custom_icons/main/version01/github.svg"
-            },
-            {
-                type: "Visual Studio Code",
-                url: "https://raw.githubusercontent.com/LucasfcBastos/custom_icons/main/version01/visual-studio-code.svg"
-            },
-            {
-                type: "HTML5",
-                url: "https://raw.githubusercontent.com/LucasfcBastos/custom_icons/main/version01/html.svg"
-            },
-            {
-                type: "CSS3",
-                url: "https://raw.githubusercontent.com/LucasfcBastos/custom_icons/main/version01/css.svg"
-            },
-            {
-                type: "JavaScript",
-                url: "https://raw.githubusercontent.com/LucasfcBastos/custom_icons/main/version01/javascript.svg"
-            },
-            {
-                type: "Vite",
-                url: "https://raw.githubusercontent.com/LucasfcBastos/custom_icons/main/version01/vite.svg"
-            },
-            {
-                type: "React",
-                url: "https://raw.githubusercontent.com/LucasfcBastos/custom_icons/main/version01/react.svg"
-            },
-            {
-                type: "Git",
-                url: "https://raw.githubusercontent.com/LucasfcBastos/custom_icons/main/version01/git.svg"
-            },
-            {
-                type: "Laragon",
-                url: "https://raw.githubusercontent.com/LucasfcBastos/custom_icons/main/version01/laragon.svg"
-            },
-            {
-                type: "Php",
-                url: "https://raw.githubusercontent.com/LucasfcBastos/custom_icons/main/version01/php.svg"
-            },
-            {
-                type: "Laravel",
-                url: "https://raw.githubusercontent.com/LucasfcBastos/custom_icons/main/version01/laravel.svg"
-            },
-            {
-                type: "MySQL",
-                url: "https://raw.githubusercontent.com/LucasfcBastos/custom_icons/main/version01/mysql.svg"
-            },
-            {
-                type: "Postman",
-                url: "https://raw.githubusercontent.com/LucasfcBastos/custom_icons/main/version01/postman.svg"
-            }
+        project_image: "https://raw.githubusercontent.com/LucasfcBastos/custom_icons/main/Portfolio/Happy%20Paws/tela.png",
+        opportunity: "O crescimento das clínicas veterinárias e pet shops evidenciou fragilidades na forma como as informações clínicas e administrativas dos animais são gerenciadas. Muitas instituições ainda dependem de registros físicos ou sistemas fragmentados, o que dificulta o acesso rápido e confiável ao histórico dos pets. Essa falta de centralização pode resultar em perda de dados, inconsistências nos registros, falhas no acompanhamento clínico e até problemas legais decorrentes de informações incompletas ou incorretas. Além disso, a desorganização compromete a eficiência operacional, aumenta o tempo de atendimento e reduz a qualidade dos serviços prestados, impactando negativamente profissionais, tutores e o bem-estar dos animais.",
+        solution: "A solução proposta consiste em um software de gestão veterinária voltado à centralização, organização e padronização das informações clínicas e administrativas dos pets. O sistema permite o registro estruturado de dados dos tutores, animais, atendimentos, serviços e profissionais envolvidos, reunindo todo o histórico do pet em um único ambiente digital seguro e acessível. Com isso, reduz-se a dependência de documentos físicos e registros dispersos, minimizando falhas operacionais e riscos relacionados à perda ou inconsistência de dados. A aplicação apoia a tomada de decisões mais assertivas, melhora a continuidade dos tratamentos e promove maior eficiência, segurança e transparência nos processos de atendimento veterinário.",
+        technologies: [
+            { type: "Word", url: "https://raw.githubusercontent.com/LucasfcBastos/custom_icons/main/version01/word.svg" },
+            { type: "Bizagi Modeler", url: "https://raw.githubusercontent.com/LucasfcBastos/custom_icons/main/version01/bizagi-modeler.svg" },
+            { type: "MySQL Workbench", url: "https://raw.githubusercontent.com/LucasfcBastos/custom_icons/main/version01/mysql-workbench.svg" },
+            { type: "Foss Flow", url: "https://raw.githubusercontent.com/LucasfcBastos/custom_icons/main/version01/foss-flow.svg" },
+            { type: "Figma", url: "https://raw.githubusercontent.com/LucasfcBastos/custom_icons/main/version01/figma.svg" },
+            { type: "Flaticon", url: "https://raw.githubusercontent.com/LucasfcBastos/custom_icons/main/version01/flaticon.svg" },
+            { type: "Storyset", url: "https://raw.githubusercontent.com/LucasfcBastos/custom_icons/main/version01/storyset.svg" },
+            { type: "GitHub", url: "https://raw.githubusercontent.com/LucasfcBastos/custom_icons/main/version01/github.svg" },
+            { type: "Visual Studio Code", url: "https://raw.githubusercontent.com/LucasfcBastos/custom_icons/main/version01/visual-studio-code.svg" },
+            { type: "HTML5", url: "https://raw.githubusercontent.com/LucasfcBastos/custom_icons/main/version01/html.svg" },
+            { type: "CSS3", url: "https://raw.githubusercontent.com/LucasfcBastos/custom_icons/main/version01/css.svg" },
+            { type: "JavaScript", url: "https://raw.githubusercontent.com/LucasfcBastos/custom_icons/main/version01/javascript.svg" },
+            { type: "Vite", url: "https://raw.githubusercontent.com/LucasfcBastos/custom_icons/main/version01/vite.svg" },
+            { type: "React", url: "https://raw.githubusercontent.com/LucasfcBastos/custom_icons/main/version01/react.svg" },
+            { type: "Git", url: "https://raw.githubusercontent.com/LucasfcBastos/custom_icons/main/version01/git.svg" },
+            { type: "Laragon", url: "https://raw.githubusercontent.com/LucasfcBastos/custom_icons/main/version01/laragon.svg" },
+            { type: "PHP", url: "https://raw.githubusercontent.com/LucasfcBastos/custom_icons/main/version01/php.svg" },
+            { type: "Laravel", url: "https://raw.githubusercontent.com/LucasfcBastos/custom_icons/main/version01/laravel.svg" },
+            { type: "MySQL", url: "https://raw.githubusercontent.com/LucasfcBastos/custom_icons/main/version01/mysql.svg" },
+            { type: "Postman", url: "https://raw.githubusercontent.com/LucasfcBastos/custom_icons/main/version01/postman.svg" }
         ],
-        saiba: [
+        learn_more: [
             {
                 url: "https://raw.githubusercontent.com/LucasfcBastos/custom_icons/main/version01/pdf.svg",
-                text_exibicao: "HappyPaws_Documentacao.pdf",
-                text_redirecionamento: "Fazer download",
-                redirecionamento: "/HappyPaws_Documentacao.pdf"
+                display_text: "HappyPaws_Documentacao.pdf",
+                redirect_text: "Fazer download",
+                redirect_url: "/HappyPaws_Documentacao.pdf"
             },
             {
                 url: "https://raw.githubusercontent.com/LucasfcBastos/custom_icons/main/version01/figma.svg",
-                text_exibicao: "PD - Happy Paws",
-                text_redirecionamento: "Acessar URL",
-                redirecionamento: "https://www.figma.com/design/8Ry5jIyUWtsBK9RFpaTFhZ/PD---Happy-Paws?node-id=335-573&t=lSIGgLln6XWzzrzc-1"
+                display_text: "PD - Happy Paws",
+                redirect_text: "Acessar URL",
+                redirect_url: "https://www.figma.com/design/8Ry5jIyUWtsBK9RFpaTFhZ/PD---Happy-Paws?node-id=335-573&t=lSIGgLln6XWzzrzc-1"
             },
             {
                 url: "https://raw.githubusercontent.com/LucasfcBastos/custom_icons/main/version01/github.svg",
-                text_exibicao: "Happy-Paws",
-                text_redirecionamento: "Acessar URL",
-                redirecionamento: "https://github.com/LucasfcBastos/Happy-Paws"
+                display_text: "Happy-Paws",
+                redirect_text: "Acessar URL",
+                redirect_url: "https://github.com/LucasfcBastos/Happy-Paws"
             }
         ],
-        versionamento: {
-            criacao: "30/09/2024",
-            versao: 2,
-            last_versao: "2.0",
-            last_date: "09/01/2026"
+        versioning: {
+            created_at: "30/09/2024",
+            version: 2,
+            latest_version: "2.0",
+            latest_date: "09/01/2026"
         }
     }
 ];
--- frontend/src/pages/AboutProject.jsx
+++ frontend/src/pages/AboutProject.jsx
@@ -10,51 +10,51 @@ function AboutProject() {
   return (
     <>
         <nav>
-            <h1>Portfolio</h1>
+            <a href="/">Portfolio</a>
             <Icon className="icon" />
         </nav>
         <div style={{padding: "1em", width: "calc(100% - 2em)", minHeight: "calc(100dvh - (5.25em + 7.5em))", marginTop: "5.25em" }}>
             <div id='main' className='project' >
                 <div className='intro'>
-                    <img src={Projeto[0].nav.url_logo} />
+                    <img src={Projeto[0].nav.logo_url} />
                     <div style={{marginLeft: "8em", marginTop: "1em"}}>
-                        <h1>{Projeto[0].nav.name_project}</h1>
+                        <h1>{Projeto[0].nav.project_name}</h1>
                         <div style={{display: "flex", gap: "1em"}}>
-                            <p>{Projeto[0].nav.tech_principal}</p>
-                            <p><strong>Setores: </strong>{Projeto[0].nav.setor}</p>
-                            <p><strong>ODS: </strong>{Projeto[0].nav.ods}</p>
+                            <p>{Projeto[0].nav.main_tech}</p>
+                            <p><strong>Setores: </strong>{Projeto[0].nav.sector}</p>
+                            <p><strong>ODS: </strong>{Projeto[0].nav.sdg}</p>
                         </div>
                     </div>
                 </div>
                 <hr style={{marginTop: "2em"}}/>
                 <div className='info'>
                     <div className='column'>
-                        <img src={Projeto[0].img_projet} className='project' />
+                        <img src={Projeto[0].project_image} className='project' />
                         <h3>Oportunidade</h3>
-                        <p>{Projeto[0].oportunidade}</p>
+                        <p>{Projeto[0].opportunity}</p>
                         <h3>Solução</h3>
-                        <p>{Projeto[0].solucao}</p>
+                        <p>{Projeto[0].solution}</p>
                     </div>
                     <div className='column'>
                         <h3>Ferramentas Utilizadas</h3>
                         <div className='camp'>
-                            {Projeto[0].tech.map((item) => (
+                            {Projeto[0].technologies.map((item) => (
                                 <StacksV img={item.url} type={item.type} />
                             ))}
                         </div>
                         <h3>Saiba Mais</h3>
                         <div>
-                            {Projeto[0].saiba.map((item) => (
-                                <IconsRedi key={item.type} img={item.url} text1={item.text_exibicao} text2={item.text_redirecionamento} redi={item.redirecionamento} />
+                            {Projeto[0].learn_more.map((item) => (
+                                <IconsRedi key={item.type} img={item.url} text1={item.display_text} text2={item.redirect_text} redi={item.redirect_url} />
                             ))}
                         </div>
                     </div>
                 </div>
                 <div className='versions'>
                     <p>
-                        Criado em {Projeto[0].versionamento.criacao} <br/>
-                        {Projeto[0].versionamento.versao} versões feitas <br/>
-                        Ultima versão {Projeto[0].versionamento.last_versao} finalizada em {Projeto[0].versionamento.last_date}
+                        Criado em {Projeto[0].versioning.created_at} <br/>
+                        {Projeto[0].versioning.version} versões feitas <br/>
+                        Ultima versão {Projeto[0].versioning.latest_version} finalizada em {Projeto[0].versioning.latest_date}
                     </p>
                 </div>
             </div>
