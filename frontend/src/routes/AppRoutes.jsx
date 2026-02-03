b810e50486678fd849180563815ad4cec36afc39
Lucas Engineer <lucas.software.engineering@gmail.com>
2026-02-03 10:05:59 -0300

    fixed

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { LandingPage } from "../pages/LandingPage";
-import { AboutProject } from "../pages/AboutProject";
-import { NotFound } from "../pages/NotFound";
+import LandingPage from "../pages/LandingPage";
+import AboutProject from "../pages/AboutProject";
+import NotFound from "../pages/NotFound";
 
 export function AppRoutes() {
   return (
