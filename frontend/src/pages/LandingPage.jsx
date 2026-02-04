dc89b19a1ead36e6ef9d93a57e169123d2e60574
Lucas Engineer <lucas.software.engineering@gmail.com>
2026-02-04 11:23:07 -0300

    responsiveness update

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -23,21 +23,21 @@ function LandingPage() {
         <h1>Portfolio</h1>
         <Icon className="icon" />
       </nav>
-      <div style={{padding: "1em", width: "calc(100% - 2em)", minHeight: "calc(100dvh - (5.25em + 7.5em))", marginTop: "5.25em" }}>
+      <div style={{padding: "1em", width: "calc(100% - 2em)", minHeight: "calc(100dvh - (5.25em + 7.5em))", marginTop: "7.5vh" }}>
         <div id='main' className='intro'>
           <div className='img_photo'>
             <Vector className="vec_photo" />
           </div>
-          <div style={{textAlign: "center", alignContent: "center"}}>
-            <span>Olá mundo, eu sou:</span><br/>
-            <span className='textAlter'>LUCAS BASTOS FRANCO</span><br/>
-            <span>Eng. de Software</span>
+          <div className='text_home' style={{textAlign: "center", alignContent: "center"}}>
+            <h1>Olá mundo, eu sou:</h1><br/>
+            <h1 className='textAlter'>LUCAS BASTOS FRANCO</h1><br/>
+            <h1>Eng. de Software</h1>
           </div>
         </div>
         <Hr type="SOBRE MIM" />
         <div id='stacks'>
-          <p style={{ textAlign: 'center'}}>Engenheiro de Software em início de carreira, apaixonado por tecnologia e inovação. Possuo experiência em projetos acadêmicos atuando em diferentes papéis, como Product Owner, Product Designer, Desenvolvedor Frontend, Desenvolvedor Backend e Quality Assurance, o que me proporcionou uma visão ampla e integrada de todo o ciclo de desenvolvimento de software. Busco oportunidades para adquirir experiência prática, gerar impacto real e crescer junto à empresa.</p>
-          <p style={{ textAlign: 'center'}}>Tenho forte capacidade de comunicação, conseguindo explicar conceitos técnicos de forma clara para públicos não técnicos, além de atuar de forma colaborativa em equipes multidisciplinares. Destaco-me pela organização, priorização de tarefas, documentação objetiva, adaptação a mudanças e aprendizado rápido. Possuo perfil analítico e criativo para resolução de problemas, tomada de decisão e identificação de causas raiz, além de experiência em ambientes que utilizam metodologias ágeis e comunicação assíncrona.</p>
+          <p style={{ textAlign: 'center'}}>Engenheiro de Software em início de carreira, apaixonado por tecnologia e inovação. Possuo experiência em projetos acadêmicos atuando em diferentes papéis, como Product Owner, Product Designer, Desenvolvedor Frontend, Desenvolvedor Backend e Quality Assurance, o que me proporcionou uma visão ampla e integrada de todo o ciclo de desenvolvimento de software. Busco oportunidades para adquirir experiência prática, gerar impacto real e crescer junto à empresa.</p><br/>
+          <p style={{ textAlign: 'center'}}>Tenho forte capacidade de comunicação, conseguindo explicar conceitos técnicos de forma clara para públicos não técnicos, além de atuar de forma colaborativa em equipes multidisciplinares. Destaco-me pela organização, priorização de tarefas, documentação objetiva, adaptação a mudanças e aprendizado rápido. Possuo perfil analítico e criativo para resolução de problemas, tomada de decisão e identificação de causas raiz, além de experiência em ambientes que utilizam metodologias ágeis e comunicação assíncrona.</p><br/>
           <p style={{ textAlign: 'center'}}>Além da área técnica, atuo como organizador e líder de eventos recorrentes, lidando com equipes e ambientes de alta pressão. Essa experiência fortaleceu minhas habilidades de liderança, mediação de conflitos, gestão de tempo e organização de agendas. Tenho vivência na resolução de imprevistos ao vivo, adaptação de conteúdos, gestão de expectativas e manutenção do engajamento do público por longos períodos, sempre mantendo o foco em resultados e colaboração.</p>
           <div className='camp-stacks'>
             {ListTech.map((item) => (
